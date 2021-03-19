00c5fc7ce2720673cc2bc0f5422524c97de61d2e
Jason <getbest@gmail.com>
2021-03-19 17:36:03 -0400

    aa

--- api.js
+++ api.js
@@ -24,7 +24,7 @@ app.listen(port, () => {
 router.get("/", (req, res) => {
   client.connect();
   client.query(
-    "SELECT * FROM transaction;",
+    "SELECT * FROM public.transaction;",
     (err, res) => {
       if (err) throw err;
       for (let row of res.rows) {
