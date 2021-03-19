ae6be8c0b0da3c1bbed1ac5ea1a39dabcae427c9
Jason <getbest@gmail.com>
2021-03-19 17:24:20 -0400

    check databse connection

--- api.js
+++ api.js
@@ -6,24 +6,36 @@ const port = process.env.PORT;
 const app = express();
 const router = express.Router();
 
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
-
 
 app.use(cors());
 
 app.listen(port, () => {
-  console.log(`Express server is running on port ${port}`)
-})
+  console.log(`Express server is running on port ${port}`);
+});
 
 router.get("/", (req, res) => {
-  res.send('Hello world');
+  client.connect();
+
+  client.query(
+    "SELECT table_schema,table_name FROM information_schema.tables;",
+    (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    }
+  );
+
+  //res.send('Hello world');
 });
 
 router.get("/json", (req, res) => {
@@ -33,24 +45,14 @@ router.get("/json", (req, res) => {
 });
 
 router.post("/replace", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   let newTrx = JSON.parse(req.body);
-  console.log(newTrx)
+  console.log(newTrx);
   fs.writeFile("./JSON/transactions.json", JSON.stringify(newTrx), finished);
 
   function finished(err) {
     console.log("JSON file replaced");
   }
-})
-
-app.use(`/`, router);;
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
 });
+
+app.use(`/`, router);
