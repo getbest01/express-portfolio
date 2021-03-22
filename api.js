d5c8e8b35a44900588a283eb061eceeb43613c81
Jason <getbest@gmail.com>
2021-03-22 12:39:52 -0400

    sdsd

--- api.js
+++ api.js
@@ -41,7 +41,7 @@ router.get("/json", async (req, res) => {
     const client = await pool.connect();
     const result = await client.query("SELECT * FROM public.transaction;");
     const results = { results: result ? result.rows : null };
-    res.send(JSON.parse(results));
+    res.send(JSON.stringify(results));
     client.release();
   } catch (err) {
     console.error(err);
