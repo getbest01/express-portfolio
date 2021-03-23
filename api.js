f0d5a398d69225aa9e007777595ee2cc2c710d83
Jason <getbest@gmail.com>
2021-03-23 14:15:18 -0400

    fixing posting

--- api.js
+++ api.js
@@ -81,7 +81,6 @@ router.post("/replace", async (req, res) => {
   try {
     console.log("post start! - show body")
     console.log(req.body);
-    console.log(req);
     let insertText = "";
 
     let newTrx = JSON.parse(req.body);
