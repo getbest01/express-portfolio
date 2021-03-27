fd9acbafc2ba88b8d4e2aec9d75fdc4d30e7fe0d
Jason <getbest@gmail.com>
2021-03-27 16:21:00 -0400

    console log request data in weather

--- api.js
+++ api.js
@@ -67,10 +67,12 @@ router.get("/json", async (req, res) => {
 
 //weather api fetch
 router.get("/weather", async (req, res) => {
+  console.log(req);
   await fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHERAPI_KEY}&q=${req.body}&days=3&alerts=yes`
   )
     .then((response) => {
+      console.log(req.body)
       return response.json();
     })
     .then((data) => res.send(data))
