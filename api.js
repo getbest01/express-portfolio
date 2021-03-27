03aa7cdc7a00de1a892cdf9445b65b20db11dedc
Jason <getbest@gmail.com>
2021-03-27 16:34:29 -0400

    change to get and use query

--- api.js
+++ api.js
@@ -66,10 +66,10 @@ router.get("/json", async (req, res) => {
 });
 
 //weather api fetch
-router.post("/weather", async (req, res) => {
+router.get("/weather", async (req, res) => {
   console.log(req);
   await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHERAPI_KEY}&q=${req.body}&days=3&alerts=yes`
+    `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHERAPI_KEY}&q=${req.query.city}&days=3&alerts=yes`
   )
     .then((response) => {
       console.log(req.body)
